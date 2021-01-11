a9d959a735f8d1fc9bea2647fff2df60d2183bb5
Aditya Rawat <56793484+adirawat450@users.noreply.github.com>
2021-01-11 12:42:32 +0530

    added my name

--- addname.nim
+++ addname.nim
@@ -1,31 +1,35 @@
-var name1:string = "Ahmed"
+var name1:string = "Aditya"
 echo name1
 
-var name2:string = "Utkarsh Singh"
+
+var name1:string = "Ahmed"
 echo name2
 
-var name3:string = "Mihai Stanciu"
+var name2:string = "Utkarsh Singh"
 echo name3
 
-var name4:string = "Taha"
+var name3:string = "Mihai Stanciu"
 echo name4
 
-var name5:string = "Prathamesh Shanbhag"
+var name4:string = "Taha"
 echo name5
 
-var name6:string = "Antonio Velazquez"
+var name5:string = "Prathamesh Shanbhag"
 echo name6
 
+var name6:string = "Antonio Velazquez"
+echo name7
+
 var whimname:string = "whimsicalities"
-echo whimname
+echo name8
 
 var whimname:string = "Eli"
-echo name 7 
+echo name9
 
 var myName:string = "Mikey San"
-echo myName
+echo name10
 
 var my_name:string = "Unni"
-echo my_name
+echo name11
 
 # from now please dont use naming convention just make the variable whatever name you want!
